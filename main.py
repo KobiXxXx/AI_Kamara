bdbc3bef605b7bfdbc97b79dc5689bc31058e16b
KobixXxX <j.schlosser.2002@web.de>
2024-06-20 13:56:10 +0100

    add

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
+from picamera2 import Picamera2
 import cv2 as cv
+import numpy as np
+import time
 
-cam = 0
-mirror = False
+#Initialisierung und Konfiguration der Kamera
+cam = Picamera2()
+camera_config = cam.create_still_configuration()
+cam.configure(camera_config)
+cam.start()
 
-cap = cv.VideoCapture(cam)
-while True:
-    ret, frame = cap.read()
-    if mirror:
-        frame = cv.flip(frame, 1)
-    cv.namedWindow('Webcam', cv.WINDOW_NORMAL)
-    cv.setWindowProperty('Webcam', cv.WND_PROP_FULLSCREEN, cv.WINDOW_FULLSCREEN)
-    cv.imshow('Webcam', frame)
-    if cv.waitKey(1) == 27:
-        break
+#Kurze Verzögerung, um die Kamera zu stabilisieren
+time.sleep(3)
+try:
+    while True:
+        #Bild aufnehmen und convertieren
+        pil_image = cam.capture_image()
+        image_np = np.array(pil_image)
+        image_bgr = cv.cvtColor(image_np, cv.COLOR_RGB2BGR)
 
-cv.destroyAllWindows('Webcam')
+        #Bild anzeigen
+        cv.imshow('Bild', image_bgr)
+
+        #Warte und Prüfe ob Taste gedrückt wurde
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
+
+except KeyboardInterrupt:
+    print('Programm wurde vom Benutzer unterbrochen')
+
+finally:
+    #Beenden der Schleife und freigeben von Ressourcen
+    cam.stop()
+    cv.destroyAllWindows()
